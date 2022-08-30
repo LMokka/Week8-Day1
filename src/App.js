87cba5b318668176573e882c34847961329fa4ef
LMokka <87783898+LMokka@users.noreply.github.com>
2022-08-30 01:32:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from "./components/Button";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    let buttons = []
+    
+
+    // Set a state for count - initial state of 0 and setCount is function to change state value of count
+    const [count, setCount] = useState(0);
+    // Set a state for names - initial state of [] and setNames is function to change state value of names
+    const [names, setNames] = useState([]);
+
+    // Function to be executed when a button is clicked
+    function handleClick(step){
+        console.log('Clicked');
+        setCount(count + step);
+    };
+
+    // Function to be exectuted when the name form is submitteed
+    function handleNameClick(e){
+        // Prevent default of refreshing page
+        e.preventDefault();
+        // Get the value from the form
+        const name = e.target.firstName.value;
+        let newNames = [...names, name]
+        // Set the state of names to be the current state of names + the new name
+        setNames(newNames)
+    }
+
+    return (
+        <>
+            <Navbar name='Coding Temple' city='Chicago'/>
+            <div className='container'>
+                <h1 className='text-center'>To-Do-List</h1>
+                <h3 className='text-center'>Enter your list item by item below</h3>
+                {buttons.map((b, i) => <Button color={b.color} step={b.step} key={i} handleClick={handleClick} />)}
+                <form onSubmit={handleNameClick}>
+                    <input type='text' className='form-control' name='firstName' />
+                    <input type='submit' value='Submit' />
+                </form>
+                {names.map((n, idx) => <p key={idx}>{n}</p>)}
+            </div>
+        </>
+    )
 }
 
 export default App;
